6c35c20fa725d25f4c98e355c05cf5c2abdd8851
lekhireddyvineeth <lekhireddyvineeth@gmail.com>
2024-07-18 04:32:55 +0000

    chnages made

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from "./Components/Projects/Projects.jsx";
 import ContactMe from "./Components/ContactMe/ContactMe.jsx";
 import Skills from "./Components/Skills/Skills.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
-import Experience from "./Components/Experience/Experience.jsx";
+
 
 const App = () => {
   return (
@@ -17,7 +17,6 @@ const App = () => {
       <DivSpace />
       <AboutMe />
       <Skills />
-      <Experience/>
       <Projects />
       <ContactMe />
       <Footer />
